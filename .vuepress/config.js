6154dd78da7cd3a120d25c503c75480606caaf15
taoyuanxu <tao.yuanxu@cgtn.com>
2020-07-07 13:30:12 +0800

    引入百度统计

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,16 @@ module.exports = {
   dest: 'dist',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
+    // 引入百度统计
+    ['script', {}, 
+            `var _hmt = _hmt || [];
+            (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?b16f225f97b3c7e88a6ecd6e49d45b37";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+            `
+    ],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
   theme: 'reco',
