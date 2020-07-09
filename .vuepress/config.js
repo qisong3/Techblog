0d45ee9d08472664e754a02d8337f0cd196f9649
taoyuanxu <tao.yuanxu@cgtn.com>
2020-07-09 22:58:06 +0800

    更新目录结构

---
+++ .vuepress/config.js
@@ -0,0 +1,32 @@
+const ThemeConfig = require('./config/theme.config')
+const PluginConfig = require('./config/plugin.config')
+
+module.exports = {
+  title: "Errison's blog center",
+  description: '不负光阴不负卿',
+  dest: 'dist',
+  plugins: PluginConfig,
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    // 引入百度统计
+    ['script', {},
+            `var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?b16f225f97b3c7e88a6ecd6e49d45b37";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
+            `
+    ],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: ThemeConfig,
+  // code theme
+  codeTheme: 'tomorrow',
+
+  markdown: {
+    lineNumbers: true
+  }
+}
---
+++ blogs/技术/Vuepress中的markdown扩展.md
@@ -0,0 +1,21 @@
+---
+title: Vuepress中的markdown扩展
+date: 2020-07-09
+tags:
+ - vuepress
+categories:
+ - 技术
+keywords:
+ - vue vuepress
+---
+
+## Java 语言历史
+
+## Java 版本特性
+
+### JDK 1.0
+### JDK 1.1
+### JDK 1.2
+### JDK 1.3
+### JDK 1.4
+
