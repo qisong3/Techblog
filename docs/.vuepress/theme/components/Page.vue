3b91b405963bec0c70899311201245a10f81662d
Errison <qisong3@gmail.com>
2020-12-09 17:36:36 +0800

    1  取消文章阅读量。

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -9,10 +9,6 @@
 
     <slot name="bottom" />
 
-    <span id="artical_visitor" class="leancloud_visitors" data-flag-title="Your Article Title">
-      <em class="post-meta-item-text">阅读量 </em>
-      <i class="leancloud-visitors-count">1000000</i>
-    </span>
   </main>
 </template>
 
